c3136a5790bfcd11f6cc34c8f90f6a566a5f9548
Jarkko Pätäri <jarkko.patari@gofore.com>
2019-06-07 12:20:14 +0300

    KJHH-1659: Korjaa auditloggerin initialisoinnin

--- vtj-service/src/main/java/fi/vm/sade/rajapinnat/vtj/AuditLogger.java
+++ vtj-service/src/main/java/fi/vm/sade/rajapinnat/vtj/AuditLogger.java
@@ -3,9 +3,7 @@ package fi.vm.sade.rajapinnat.vtj;
 import fi.vm.sade.auditlog.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
-@Component
 public class AuditLogger {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AuditLogger.class);
