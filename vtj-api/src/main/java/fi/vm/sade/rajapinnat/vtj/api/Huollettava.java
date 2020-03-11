63a62e3c5adc351243e84e1e14e12c0c49743e63
Heikki Hulkko <heikki.hulkko@reaktor.com>
2020-03-11 17:15:56 +0200

    Lisää vaaditut setterit

--- vtj-api/src/main/java/fi/vm/sade/rajapinnat/vtj/api/Huollettava.java
+++ vtj-api/src/main/java/fi/vm/sade/rajapinnat/vtj/api/Huollettava.java
@@ -5,9 +5,7 @@ public class Huollettava {
     private String sukunimi;
     private String hetu;
 
-    public Huollettava() {
-
-    }
+    public Huollettava() { }
 
     public Huollettava(String etunimet, String sukunimi, String hetu) {
         this.etunimet = etunimet;
@@ -15,12 +13,24 @@ public class Huollettava {
         this.hetu = hetu;
     }
 
+    public void setEtunimet(String etunimet) {
+        this.etunimet = etunimet;
+    }
+
     public String getEtunimet() {
        return etunimet;
     }
 
+    public void setSukunimi(String sukunimi) {
+        this.sukunimi = sukunimi;
+    }
+
     public String getSukunimi() {
         return sukunimi;
+    }
+
+    public void setHetu(String hetu) {
+        this.hetu = hetu;
     }
 
     public String getHetu() {
