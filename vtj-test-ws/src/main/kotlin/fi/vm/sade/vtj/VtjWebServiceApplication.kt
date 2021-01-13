b9ac3b93e6739e4a11d966080fdd85ac1f9e3576
Jani Kaarela <jani.kaarela@gmail.com>
2021-01-13 12:14:03 +0200

    Test-WS:n kontitus; salaus ja autentikointi.

--- vtj-test-ws/src/main/kotlin/fi/vm/sade/vtj/VtjWebServiceApplication.kt
+++ vtj-test-ws/src/main/kotlin/fi/vm/sade/vtj/VtjWebServiceApplication.kt
@@ -2,10 +2,8 @@ package fi.vm.sade.vtj
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.ws.config.annotation.EnableWs
 
 @SpringBootApplication
-@EnableWs
 class VtjWebServiceApplication
 
 fun main(args: Array<String>) {
---
+++ vtj-test-ws/src/main/kotlin/fi/vm/sade/vtj/config/SecurityConfig.kt
@@ -0,0 +1,43 @@
+package fi.vm.sade.vtj.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.authority.AuthorityUtils
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import java.lang.Exception
+import kotlin.jvm.Throws
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig: WebSecurityConfigurerAdapter() {
+
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http.authorizeRequests().anyRequest().authenticated()
+                .and()
+                .x509()
+                .subjectPrincipalRegex("CN=(.*?)(?:,|$)")
+                .userDetailsService(userDetailsService())
+    }
+
+    override fun userDetailsService(): UserDetailsService {
+        return object: UserDetailsService {
+            override fun loadUserByUsername(username: String?): UserDetails {
+                if (VTJ_SERVICE_USERNAME == username) {
+                    return User(
+                            username,
+                            "", // serttiautentikointi; ei salasanaa
+                            AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER"))
+                }
+                throw UsernameNotFoundException("No such user: $username")
+            }
+        }
+    }
+}
+
+const val VTJ_SERVICE_USERNAME = "vtj-service"
