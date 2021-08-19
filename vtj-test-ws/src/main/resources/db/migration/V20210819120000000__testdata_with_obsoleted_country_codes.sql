e1a03d845c7a43904f683bf0e5dfbba2d2b681e7
Lauri Arimo <lauri.arimo@wunderdog.fi>
2021-08-19 12:05:48 +0300

    KJHH-2024: Add test data with obsoleted country codes

---
+++ vtj-test-ws/src/main/resources/db/migration/V20210819120000000__testdata_with_obsoleted_country_codes.sql
@@ -0,0 +1,4 @@
+-- Testihenkiloitä vanhentuneilla maakoodeilla (KJHH-2024)
+INSERT INTO henkilo (henkilotunnus, etunimet, kutsumanimi, sukunimi, sukupuoli, sahkoposti, kansalaisuus, kotimainen_osoite_id) VALUES
+   ('261076-994H', 'Adelina Testi',              'Adelina',       'Häyrinen-Testi',          'NAINEN', null, '736', null),
+   ('130662-996B', 'Moa Testi',                  'Moa',           'Nyyssönen-Testi',         'NAINEN', null, '230', null);
