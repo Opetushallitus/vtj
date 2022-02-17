bc649e2b7a49f6bbb42c7ff615e20eb5c777ee2b
Lauri Arimo <lauri.arimo@wunderdog.fi>
2022-02-17 16:19:11 +0200

    KJHH-2030: Insert fake data for testing person creation UI

---
+++ vtj-test-ws/src/main/resources/db/migration/V20220217120000000__insert_fake_data.sql
@@ -0,0 +1,24 @@
+--
+-- Fake data for testing KJHH-2030
+--
+INSERT INTO henkilo (henkilotunnus, etunimet, kutsumanimi, sukunimi, sukupuoli, sahkoposti, kansalaisuus, kotimainen_osoite_id) VALUES
+('161052-777V', 'testi-3', 'testi-3', 'testi-3', 'MIES', null, 246, null),
+('240156-5095', 'testi-1', 'testi-1', 'testi-1', 'MIES', null, 246, null),
+('011011-403J', 'testi-2', 'testi-2', 'testi-2', 'MIES', null, 246, null),
+('210164-5099', 'testi-4', 'testi-4', 'testi-4', 'MIES', null, 246, null),
+('060217-497W', 'testi-5', 'testi-5', 'testi-5', 'MIES', null, 246, null),
+('060975-1090', 'testi-6', 'testi-6', 'testi-6', 'MIES', null, 246, null),
+('031132-3799', 'testi-7', 'testi-7', 'testi-7', 'MIES', null, 246, null),
+('290706-373X', 'testi-8', 'testi-8', 'testi-8', 'MIES', null, 246, null),
+('050404-045X', 'testi-9', 'testi-9', 'testi-9', 'MIES', null, 246, null),
+('270888-2034', 'testi-10', 'testi-10', 'testi-10', 'MIES', null, 246, null),
+('270717-0480', 'testi-11', 'testi-11', 'testi-11', 'NAINEN', null, 246, null),
+('240563-278R', 'testi-12', 'testi-12', 'testi-12', 'NAINEN', null, 246, null),
+('120270-5527', 'testi-13', 'testi-13', 'testi-13', 'NAINEN', null, 246, null),
+('211150-692M', 'testi-14', 'testi-14', 'testi-14', 'NAINEN', null, 246, null),
+('070240-308C', 'testi-15', 'testi-15', 'testi-15', 'NAINEN', null, 246, null),
+('140767-694B', 'testi-16', 'testi-16', 'testi-16', 'NAINEN', null, 246, null),
+('130596-440C', 'testi-17', 'testi-17', 'testi-17', 'NAINEN', null, 246, null),
+('130279-282F', 'testi-18', 'testi-18', 'testi-18', 'NAINEN', null, 246, null),
+('020210-8009', 'testi-19', 'testi-19', 'testi-19', 'NAINEN', null, 246, null),
+('090757-502B', 'testi-20', 'testi-20', 'testi-20', 'NAINEN', null, 246, null);
